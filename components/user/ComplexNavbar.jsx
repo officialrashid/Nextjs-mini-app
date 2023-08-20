35ac2ded5fb0b4882be3f17adc43d36ff6ccf66f
officialrashid <muhammedrashi59@gmail.com>
2023-08-20 17:48:16 +0530

    changes trying cicd

--- components/user/ComplexNavbar.jsx
+++ components/user/ComplexNavbar.jsx
@@ -200,7 +200,7 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: 'none', sm: 'block',marginLeft:50 } }}
           >
-            Shop
+            Shopping??
           </Typography>
           <Typography
             variant="h6"
